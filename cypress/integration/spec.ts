ebf4f601c5c5db97eecc330dee82f0df565eddc9
christi4n <christian@mydevfactory.net>
2022-05-26 00:28:12 +0200

    [TASK] Rename test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,4 +1,4 @@
-describe('My First Test', () => {
+describe('Cypress.io test', () => {
   it('Visits the main page', () => {
     cy.visit('/')
     cy.title().should('eq', 'Azure Cloud Consultant / DevOps Engineer / Christian BELLET')
